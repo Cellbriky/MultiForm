9304d572fa558ffa59cdae19d8efafff2cffc9e7
Israel <cellbriky12@gmail.com>
2026-09-13 06:40:01 -0700

    fetch change layout

--- app/styles/Step1.ts
+++ app/styles/Step1.ts
@@ -1,6 +1,6 @@
 import { Box, styled } from "@mui/material"
 export const PageContainer = styled(Box)(({ theme }) => ({
-    maxWidth: theme.tokens.layout.container.form,
+    maxWidth: theme.tokens.layoutToken.container.form,
     width: "100%",
     backgroundColor: "#fff",
     
