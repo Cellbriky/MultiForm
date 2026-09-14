e25e2ba8105940e67259bf4af1df26c81fba52b7
Israel <cellbriky12@gmail.com>
2026-09-14 04:25:39 -0700

    change globalstyle

--- app/feature/footer/submit.ts
+++ app/feature/footer/submit.ts
@@ -1,6 +1,5 @@
 import { Box, styled } from '@mui/material';
 export const Container = (styled)(Box)(({ theme }) => ({
-    marginTop:theme.tokens.form.sectionGap['2xl'],
     backgroundColor: "#fff",
     padding: theme.tokens.padding.container.mobile,
     display: "flex",
